6208645c2c26e49e9dc0877987e7e74659906b4b
Tanay Rishi <Tanay.Rishi@fdmgroup.com>
2024-04-03 12:23:52 +0800

    Add command line startup runner to automatically create an admin user upon app startup

---
+++ todolist/src/main/java/com/fdmgroup/todolist/TodolistCommandLineAppStartupRunner.java
@@ -0,0 +1,43 @@
+package com.fdmgroup.todolist;
+
+/**
+ * This class is used to create an admin user in the database when the app start up is performed.
+ * The admin will be used to perform meta operations for the To Do list application, such as 
+ * creating new categories for task classification.
+ */
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.fdmgroup.todolist.model.User;
+import com.fdmgroup.todolist.service.UserService;
+
+@Profile("!test")
+@Component
+public class TodolistCommandLineAppStartupRunner implements CommandLineRunner {
+	
+	@Autowired
+	UserService userService;
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@Value("${USERNAME}")
+	private String adminUsername;
+	
+	@Value("${PASSWORD}")
+	private String adminPassword;
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		String adminRoles = "ROLE_USER,ROLE_ADMIN";
+		User admin = new User(adminUsername, encoder.encode(adminPassword), adminRoles);
+        userService.createUser(admin);
+	}
+
+}
